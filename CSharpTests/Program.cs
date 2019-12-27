f0e043bf16e0d860f4f82a7d464a550924efa3e4
ilushareg <ilushareg@gmail.com>
2019-12-27 13:36:55 +0300

    initial commit

---
+++ CSharpTests/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpTests
+{
+    public class Act
+    {
+        public Act()
+        {
+            Do(delegate (string s) { Console.WriteLine(s); return 5; });
+            DoVoid(delegate { Console.WriteLine("DoVoid"); return; });
+
+            //Func<int> a = new Func<int>{return 1; };
+
+        }
+
+    void DoVoid(Action dv)
+        {
+            dv();
+        }
+
+        void Do(Func<string, int> myMethodName)
+        {
+            int i = myMethodName("My String");
+
+        }
+
+    }
+
+    class Program
+    {
+        public static int tstatic = 0;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Thread[] tList = new Thread[5];
+            {
+                for (int i = 0; i < tList.Length; i++)
+                {
+                    Thread thr = new Thread(new ParameterizedThreadStart(delegate (Object o)
+                    {
+                        try
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("Hello Thread {0}", (int)o);
+                                Thread.Sleep(100);
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine("Exception caught {0}", e.Message);
+                        }
+
+                    }));
+                    //Thread thr = new Thread(new ThreadStart(delegate { tstatic = Thread.CurrentThread.ManagedThreadId; Console.WriteLine(tstatic); tstatic = 99; Thread.Sleep(100); Console.WriteLine(tstatic); }));
+                    tList[i] = thr;
+                }
+            }
+
+            for (int i = 0; i < tList.Length; i++)
+            {
+                tList[i].Start(i);
+            }
+            for (int i = 0; i < tList.Length; i++)
+            {
+                tList[i].Abort();
+            }
+
+
+        }
+    }
+}
